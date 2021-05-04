7399646adefa05247f3ddd07afb3c351407c2fde
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-05-04 11:18:57 -0700

    Add links to routes
    Co-authored-by: Joseph Giunta <jgnohack@gmail.com>
    Co-authored-by: Scott O'Neil <scottieoneil@gmail.com>

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <>
       <h1>Learning Log</h1>
+      <p><Link to="/">Home</Link> | <Link to="/signin">Sign In</Link></p>
     </>
   );
 }
