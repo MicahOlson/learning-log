02ca464631c5a18e23838842ec2f6c8e199072f7
Micah L. Olson <76067607+MicahOlson@users.noreply.github.com>
2021-05-04 11:14:59 -0700

    Add router to App
    Co-authored-by: Joseph Giunta <jgnohack@gmail.com>
    Co-authored-by: Scott O'Neil <scottieoneil@gmail.com>

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './Header';
 import LogControl from './LogControl';
+import Signin from './Signin';
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
-      <LogControl />
-    </>
+      <Switch>
+        <Route path="/signin">
+          <Signin />
+        </Route>
+        <Route path="/">
+          <LogControl />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
